61dc4d8be11f76859f3f1321c6594ff2410df0bd
aksen510 <510cara@naver.com>
2022-06-23 05:37:42 +0900

    [FEAT] Edit UI of WeatherFragment

--- app/src/main/java/com/example/walgwalg_front_android/home/WeatherFragment.java
+++ app/src/main/java/com/example/walgwalg_front_android/home/WeatherFragment.java
@@ -6,8 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.Navigation;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -18,14 +21,20 @@ import android.view.ViewGroup;
 import com.example.walgwalg_front_android.R;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabLayout;
 
 import java.util.Objects;
 
 public class WeatherFragment extends Fragment {
 
-    private MaterialToolbar tb_weather;
+//    private MaterialToolbar tb_weather;
     FragmentManager fragmentManager;
 //    public FragmentManager fragmentManager = HomeFragment.fragmentManager.findFragmentById(R.id.homeFragment);
+    TodayWeatherFragment todayWeatherFragment;
+    WeekWeatherFragment weekWeatherFragment;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private TabPagerAdapter tabPagerAdapter;
 
 
     public WeatherFragment() {
@@ -51,39 +60,47 @@ public class WeatherFragment extends Fragment {
         hideBottomNavigation(true);
         setHasOptionsMenu(true);
 
-        fragmentManager = getActivity().getSupportFragmentManager();
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabPagerAdapter = new TabPagerAdapter(getActivity().getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        tabPagerAdapter.addFragment(new TodayWeatherFragment(), "Today");
+        tabPagerAdapter.addFragment(new WeekWeatherFragment(), "Week");
+        viewPager.setAdapter(tabPagerAdapter);
+
 
         // Toolbar 활성화
 
 //        fragmentManager = getActivity().getSupportFragmentManager();
 //        fragmentManager.beginTransaction().replace(R.id.weatherFragment, new HomeFragment()).addToBackStack(null).commit();
 
-        ((AppCompatActivity)getActivity()).setSupportActionBar(tb_weather);
-        Objects.requireNonNull(((AppCompatActivity)getActivity()).getSupportActionBar()).setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼 생성
-        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle(null); // Toolbar 제목 제거
+//        ((AppCompatActivity)getActivity()).setSupportActionBar(tb_weather);
+//        Objects.requireNonNull(((AppCompatActivity)getActivity()).getSupportActionBar()).setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼 생성
+//        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle(null); // Toolbar 제목 제거
 
         return view;
     }
 
     // Toolbar 뒤로가기 버튼 활성화 코드
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home: { //toolbar의 back키 눌렀을 때 동작
-                Log.d("WeatherFragment","BackButton");
-//                Navigation.findNavController(getView()).navigate(R.id.action_weatherFragment_to_homeFragment);
-//                fragmentManager = getActivity().getSupportFragmentManager();
-//                fragmentManager.beginTransaction().remove(WeatherFragment.this).commit();
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case android.R.id.home: { //toolbar의 back키 눌렀을 때 동작
+//                Log.d("WeatherFragment","BackButton");
+////                Navigation.findNavController(getView()).navigate(R.id.action_weatherFragment_to_homeFragment);
+////                fragmentManager = getActivity().getSupportFragmentManager();
+////                fragmentManager.beginTransaction().remove(WeatherFragment.this).commit();
+////                fragmentManager.popBackStack();
 //                fragmentManager.popBackStack();
-                fragmentManager.popBackStack();
-                return true;
-            }
-        }
-        return super.onOptionsItemSelected(item);
-    }
+//                return true;
+//            }
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 
     public void init(View view) {
-        tb_weather = view.findViewById(R.id.tb_weather);
+//        tb_weather = view.findViewById(R.id.tb_weather);
+        tabLayout = view.findViewById(R.id.tablayout);
+        viewPager = view.findViewById(R.id.viewpager);
     }
 
     @Override
