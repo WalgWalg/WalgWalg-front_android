7dbbb04ceeb893dbb06dccc3f868159d58388e38
Junha Hwang <junha5021301@kakao.com>
2022-06-24 00:09:58 +0900

    [FEAT] Separated SharedPreferences function

---
+++ app/src/main/java/com/example/walgwalg_front_android/member/PreferenceHelper.java
@@ -0,0 +1,72 @@
+package com.example.walgwalg_front_android.member;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class PreferenceHelper {
+
+    // TODO: token(access, refresh) getter 만들어야됨. - 20220621-18:21
+
+    private final String USERID = "userid";
+    private final String PASSWORD = "password";
+    private final String DATETIME = "dateTime";
+    private final String ACCESSTOKEN = "accessToken";
+    private final String REFRESHTOKEN = "refreshToken";
+    private SharedPreferences user_prefs;
+    private Context context;
+
+    public PreferenceHelper(Context context) {
+        user_prefs = context.getSharedPreferences("shared", 0);
+        this.context = context;
+    }
+
+    public void saveUserid(String userId) {
+        SharedPreferences.Editor edit = user_prefs.edit();
+        edit.putString(USERID, userId);
+        edit.apply();
+    }
+
+    public String getUserid() {
+        return user_prefs.getString(USERID, "");
+    }
+
+    public void savePassword(String passWord) {
+        SharedPreferences.Editor edit = user_prefs.edit();
+        edit.putString(PASSWORD, passWord);
+        edit.apply();
+    }
+
+    public String getPassword() {
+        return user_prefs.getString(PASSWORD, "");
+    }
+
+    public void saveDateTime(String dateTime) {
+        SharedPreferences.Editor edit = user_prefs.edit();
+        edit.putString(DATETIME, dateTime);
+        edit.apply();
+    }
+
+    public String getDateTime() {
+        return user_prefs.getString(DATETIME, "");
+    }
+
+    public void saveAccessToken(String accessToken) {
+        SharedPreferences.Editor edit = user_prefs.edit();
+        edit.putString(ACCESSTOKEN, accessToken);
+        edit.apply();
+    }
+
+    public String getAccessToken() {
+        return user_prefs.getString(ACCESSTOKEN, "");
+    }
+
+    public void saveRefreshToken(String refreshToken) {
+        SharedPreferences.Editor edit = user_prefs.edit();
+        edit.putString(REFRESHTOKEN, refreshToken);
+        edit.apply();
+    }
+
+    public String getRefreshToken() {
+        return user_prefs.getString(REFRESHTOKEN, "");
+    }
+}
