cb71680fa8f37d4a1a7a48f2c70303deefec96ff
aksen510 <510cara@naver.com>
2022-06-27 23:41:06 +0900

    [FEAT] Add Model

---
+++ app/src/main/java/com/example/walgwalg_front_android/model/Current.java
@@ -0,0 +1,12 @@
+package com.example.walgwalg_front_android.model;
+
+public class Current {
+    private int dt;
+    private int sunrise;
+    private int sunset;
+    private double temp;
+    private double feels_like;
+    private int pressure;
+    private int huminity;
+
+}
---
+++ app/src/main/java/com/example/walgwalg_front_android/model/Hourly.java
@@ -0,0 +1,118 @@
+package com.example.walgwalg_front_android.model;
+
+import java.util.List;
+
+public class Hourly {
+
+    private int dt;
+    private double temp;
+    private double feels_like;
+    private int pressure;
+    private int humidity;
+    private double dew_point;
+    private int uvi;
+    private int clouds;
+    private int visibility;
+    private Wind wind;
+    private int pop;
+    private List<Weather> weather;
+
+    public Hourly() {
+    }
+
+    public int getDt() {
+        return dt;
+    }
+
+    public void setDt(int dt) {
+        this.dt = dt;
+    }
+
+    public double getTemp() {
+        return temp;
+    }
+
+    public void setTemp(double temp) {
+        this.temp = temp;
+    }
+
+    public double getFeels_like() {
+        return feels_like;
+    }
+
+    public void setFeels_like(double feels_like) {
+        this.feels_like = feels_like;
+    }
+
+    public int getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(int pressure) {
+        this.pressure = pressure;
+    }
+
+    public int getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(int humidity) {
+        this.humidity = humidity;
+    }
+
+    public double getDew_point() {
+        return dew_point;
+    }
+
+    public void setDew_point(double dew_point) {
+        this.dew_point = dew_point;
+    }
+
+    public int getUvi() {
+        return uvi;
+    }
+
+    public void setUvi(int uvi) {
+        this.uvi = uvi;
+    }
+
+    public int getClouds() {
+        return clouds;
+    }
+
+    public void setClouds(int clouds) {
+        this.clouds = clouds;
+    }
+
+    public int getVisibility() {
+        return visibility;
+    }
+
+    public void setVisibility(int visibility) {
+        this.visibility = visibility;
+    }
+
+    public Wind getWind() {
+        return wind;
+    }
+
+    public void setWind(Wind wind) {
+        this.wind = wind;
+    }
+
+    public int getPop() {
+        return pop;
+    }
+
+    public void setPop(int pop) {
+        this.pop = pop;
+    }
+
+    public List<Weather> getWeather() {
+        return weather;
+    }
+
+    public void setWeather(List<Weather> weather) {
+        this.weather = weather;
+    }
+}
