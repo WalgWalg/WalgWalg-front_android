336759a3937735d7769df99d0b97b1975788c442
daeunkim23 <ekqhddl23@naver.com>
2022-07-06 02:18:33 +0900

    [FIX] RecordActivity permmision error fix

--- app/src/main/java/com/example/walgwalg_front_android/location/RecordActivity.java
+++ app/src/main/java/com/example/walgwalg_front_android/location/RecordActivity.java
@@ -1,13 +1,16 @@
 package com.example.walgwalg_front_android.location;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -70,6 +73,10 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class RecordActivity extends AppCompatActivity implements AutoPermissionsListener, SensorEventListener {
+
+    private static final int GPS_ENABLE_REQUEST_CODE = 2001;
+    private static final int PERMISSIONS_REQUEST_CODE = 100;
+    String[] REQUIRED_PERMISSIONS = {Manifest.permission.ACCESS_FINE_LOCATION};
     private TextView btn_location, txt_km, txt_time, txt_step, txt_kcal;
     private Button btn_end;
     private SlidingDrawer sliding_drawer,select_drawer;
@@ -80,7 +87,7 @@ public class RecordActivity extends AppCompatActivity implements AutoPermissions
     private List<Polyline> polylines = new ArrayList();
     private LatLng startLatLng = new LatLng(0, 0);
     private LatLng endLatLng = new LatLng(0, 0);
-    private static double latitude,longitude;
+    private double latitude,longitude;
 
     private static int distance, predistance = 0;
     private Timer timer;
@@ -93,9 +100,7 @@ public class RecordActivity extends AppCompatActivity implements AutoPermissions
     private int one = 300;
     private static int kcals = 0;
     private double time = 0.000;
-    //현재 걸음 수
     private static int mSteps = 0;
-    //리스너가 등록되고 난 후의 step count
     private int mCounterSteps = 0;
     private SensorManager sensorManager;
     private Sensor stepCountSensor;
@@ -147,13 +152,25 @@ public class RecordActivity extends AppCompatActivity implements AutoPermissions
         sliding_drawer.setVisibility(View.GONE);
         select_drawer = findViewById(R.id.select_drawer);
         select_drawer.setVisibility(View.VISIBLE);
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        stepCountSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         preferenceHelper = new PreferenceHelper(getApplicationContext());
         apiClient=new ApiClient(usertoken);
 
-        // 디바이스에 걸음 센서의 존재 여부 체크
+        if (!checkLocationServicesStatus()) {
+            showDialogForLocationServiceSetting();
+        }else {
+            checkRunTimePermission();
+        }
+        // 활동 퍼미션 체크
+        if(ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_DENIED){
+
+            requestPermissions(new String[]{Manifest.permission.ACTIVITY_RECOGNITION}, 0);
+        }
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        stepCountSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+
         if (stepCountSensor == null) {
             Toast.makeText(getApplicationContext(), "No Step Sensor", Toast.LENGTH_SHORT).show();
         }
@@ -167,12 +184,10 @@ public class RecordActivity extends AppCompatActivity implements AutoPermissions
         kcal = new Timer();
         startKcal();
 
-//        gpsapi=new Timer();
 
         manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        //폴리라인 그리기
         polyline = new MapPolyline();
-        polyline.setLineColor(Color.RED); // Polyline 컬러 지정.
+        polyline.setLineColor(Color.RED);
         gpsListener = new GPSListener();
 
         try {
@@ -212,8 +227,6 @@ public class RecordActivity extends AppCompatActivity implements AutoPermissions
                 String region;
 
                 try {
-//                    first_latitude=35.8767887;    //대구
-//                    first_longitude=128.5962455;
                     Log.d("test",String.valueOf(latitude)+"  "+String.valueOf(longitude));
                     address = g.getFromLocation(latitude,longitude,10);
                 } catch (IOException e) {
@@ -247,12 +260,6 @@ public class RecordActivity extends AppCompatActivity implements AutoPermissions
                         Log.d("RecordActivity", dataList.tolist());
 
                         dataInfo=dataList.list;
-//                        for (int i = 0; i < dataInfo.size(); i++) {
-//                            if("35.5459538599".equals(dataInfo.get(i).getLatitude())){
-//                                dataresult.add(dataInfo.get(i));
-//                            }
-//                        }
-
                         park_adapter = new Park_Adapter(getApplicationContext(), dataInfo);
 
                         recyclerView.setAdapter(park_adapter);
@@ -295,7 +302,6 @@ public class RecordActivity extends AppCompatActivity implements AutoPermissions
                 kcalTask.cancel();
                 manager.removeUpdates(gpsListener);
 
-                //캡쳐
                 map.snapshot(new GoogleMap.SnapshotReadyCallback() {
                     @Override
                     public void onSnapshotReady(Bitmap bitmap) {
@@ -308,7 +314,7 @@ public class RecordActivity extends AppCompatActivity implements AutoPermissions
                         Log.i("finish", Environment.getExternalStorageDirectory() + "/Pictures");
                         try {
                             FileOutputStream os = new FileOutputStream(infile);
-                            bitmap.compress(Bitmap.CompressFormat.PNG, 100, os); //PNG파일로 만들기
+                            bitmap.compress(Bitmap.CompressFormat.PNG, 100, os);
 
                             RequestBody requestFile=RequestBody.create(MediaType.parse("image/png"),infile);
                             file = MultipartBody.Part.createFormData("file", infile.getName(), requestFile);
@@ -357,20 +363,15 @@ public class RecordActivity extends AppCompatActivity implements AutoPermissions
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
-//                        if (file != null) {
-//                            sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(file)));
-//                            Toast.makeText(getApplicationContext(), "저장성공", Toast.LENGTH_SHORT).show();
-//                        }
 
                     }
                 });
 
-//
 
             }
         });
 
-    }//oncreate-end
+    }
 
     public void postintent_parkname(String title){
         park_name=title;
@@ -428,8 +429,8 @@ public class RecordActivity extends AppCompatActivity implements AutoPermissions
             }
         });
     }
-private GpsInterface gpsInterface = apiClient.getClient().create(GpsInterface.class);
-    //gpsapi
+    private GpsInterface gpsInterface = apiClient.getClient().create(GpsInterface.class);
+
     private void gpsapi() {
         gpsapiTask = new TimerTask() {
             @Override
@@ -767,13 +768,6 @@ private GpsInterface gpsInterface = apiClient.getClient().create(GpsInterface.cl
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        AutoPermissions.Companion.parsePermissions(RecordActivity.this, requestCode, permissions, this);
-//        Toast.makeText(getApplicationContext(), "requestCode : "+requestCode+"  permissions : "+permissions+"  grantResults :"+grantResults, Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
     public void onDenied(int requestCode, String[] permissions) {
 //        Toast.makeText(getApplicationContext(),"permissions denied : " + permissions.length, Toast.LENGTH_SHORT).show();
     }
@@ -782,6 +776,121 @@ private GpsInterface gpsInterface = apiClient.getClient().create(GpsInterface.cl
     public void onGranted(int requestCode, String[] permissions) {
 //        Toast.makeText(getApplicationContext(),"permissions granted : " + permissions.length, Toast.LENGTH_SHORT).show();
     }
+
+    public boolean checkLocationServicesStatus() {
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case GPS_ENABLE_REQUEST_CODE:
+                //사용자가 GPS 활성 시켰는지 검사
+                if (checkLocationServicesStatus()) {
+                    if (checkLocationServicesStatus()) {
+                        Log.d("@@@", "onActivityResult : GPS 활성화 되있음");
+                        checkRunTimePermission();
+                        return;
+                    }
+                }
+                break;
+        }
+    }
+    //여기부터는 GPS 활성화를 위한 메소드들
+    private void showDialogForLocationServiceSetting() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(RecordActivity.this);
+        builder.setTitle("위치 서비스 비활성화");
+        builder.setMessage("앱을 사용하기 위해서는 위치 서비스가 필요합니다.\n"
+                + "위치 설정을 수정하시겠습니까?");
+        builder.setCancelable(true);
+        builder.setPositiveButton("설정", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                Intent callGPSSettingIntent
+                        = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivityForResult(callGPSSettingIntent, GPS_ENABLE_REQUEST_CODE);
+            }
+        });
+        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.cancel();
+            }
+        });
+        builder.create().show();
+    }
+    void checkRunTimePermission(){
+
+        //런타임 퍼미션 처리
+        // 1. 위치 퍼미션을 가지고 있는지 체크합니다.
+        int hasFineLocationPermission = ContextCompat.checkSelfPermission(RecordActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED ) {
+            // 2. 이미 퍼미션을 가지고 있다면
+            // ( 안드로이드 6.0 이하 버전은 런타임 퍼미션이 필요없기 때문에 이미 허용된 걸로 인식합니다.)
+            // 3.  위치 값을 가져올 수 있음
+
+        } else {  //2. 퍼미션 요청을 허용한 적이 없다면 퍼미션 요청이 필요합니다. 2가지 경우(3-1, 4-1)가 있습니다.
+            // 3-1. 사용자가 퍼미션 거부를 한 적이 있는 경우에는
+            if (ActivityCompat.shouldShowRequestPermissionRationale(RecordActivity.this, REQUIRED_PERMISSIONS[0])) {
+                // 3-2. 요청을 진행하기 전에 사용자가에게 퍼미션이 필요한 이유를 설명해줄 필요가 있습니다.
+                Toast.makeText(getApplicationContext(), "이 앱을 실행하려면 위치 접근 권한이 필요합니다.", Toast.LENGTH_LONG).show();
+                // 3-3. 사용자게에 퍼미션 요청을 합니다. 요청 결과는 onRequestPermissionResult에서 수신됩니다.
+                ActivityCompat.requestPermissions(RecordActivity.this, REQUIRED_PERMISSIONS,
+                        PERMISSIONS_REQUEST_CODE);
+            } else {
+                // 4-1. 사용자가 퍼미션 거부를 한 적이 없는 경우에는 퍼미션 요청을 바로 합니다.
+                // 요청 결과는 onRequestPermissionResult에서 수신됩니다.
+                ActivityCompat.requestPermissions(RecordActivity.this, REQUIRED_PERMISSIONS,
+                        PERMISSIONS_REQUEST_CODE);
+            }
+        }
+    }
+
+    private void onFinishReverseGeoCoding(String result) {
+//        Toast.makeText(LocationDemoActivity.this, "Reverse Geo-coding : " + result, Toast.LENGTH_SHORT).show();
+    }
+
+    // ActivityCompat.requestPermissions를 사용한 퍼미션 요청의 결과를 리턴받는 메소드
+    @Override
+    public void onRequestPermissionsResult(int permsRequestCode, @NonNull String[] permissions, @NonNull int[] grandResults) {
+        super.onRequestPermissionsResult(permsRequestCode, permissions, grandResults);
+        AutoPermissions.Companion.parsePermissions(RecordActivity.this, permsRequestCode, permissions, this);
+
+        if (permsRequestCode == PERMISSIONS_REQUEST_CODE && grandResults.length == REQUIRED_PERMISSIONS.length) {
+
+            // 요청 코드가 PERMISSIONS_REQUEST_CODE 이고, 요청한 퍼미션 개수만큼 수신되었다면
+            boolean check_result = true;
+
+            // 모든 퍼미션을 허용했는지 체크합니다.
+            for (int result : grandResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    check_result = false;
+                    break;
+                }
+            }
+
+            if (check_result) {
+                Log.d("@@@", "start");
+                //위치 값을 가져올 수 있음
+
+            } else {
+                // 거부한 퍼미션이 있다면 앱을 사용할 수 없는 이유를 설명해주고 앱을 종료합니다.2 가지 경우가 있다
+                if (ActivityCompat.shouldShowRequestPermissionRationale(RecordActivity.this, REQUIRED_PERMISSIONS[0])) {
+                    Toast.makeText(getApplicationContext(), "퍼미션이 거부되었습니다. 앱을 다시 실행하여 퍼미션을 허용해주세요.", Toast.LENGTH_LONG).show();
+                    finish();
+                } else {
+                    Toast.makeText(getApplicationContext(), "퍼미션이 거부되었습니다. 설정(앱 정보)에서 퍼미션을 허용해야 합니다. ", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
 }
 
 
